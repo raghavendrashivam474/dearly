aca0a0ba6f1f7b26558d77386891d2225fb57c38
Raghavendra Singh <raghavendrashivam474@gmail.com>
2026-06-17 23:36:24 +0530

    fix: add prisma generate to build script for vercel

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,8 +1,10 @@
 import { PrismaNeon } from "@prisma/adapter-neon"
 
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 const { PrismaClient } = require("@prisma/client")
 
 const globalForPrisma = globalThis as unknown as {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   prisma: any | undefined
 }
 
